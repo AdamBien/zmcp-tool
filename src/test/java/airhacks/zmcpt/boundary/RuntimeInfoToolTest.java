49aae4566f944fbbdc997a3e7d3d98d6564671cb
Adam Bien <abien@adam-bien.com>
2025-07-05 14:32:03 +0200

    UT ToolSpec existence

--- src/test/java/airhacks/zmcpt/boundary/RuntimeInfoToolTest.java
+++ src/test/java/airhacks/zmcpt/boundary/RuntimeInfoToolTest.java
@@ -6,11 +6,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RuntimeInfoToolTest {
     
     @Test
-    void invokeTool() {
+    void responseContainsKeys() {
         var tool = new RuntimeInfoTool();
         var response = tool.apply(null);
         assertThat(response).isNotNull().as("response should not be null");
         assertThat(response.get("content")).isNotNull().as("content should not be null");
         assertThat(response.get("error")).isNotNull().as("error should not be null");
+    }
+
+
+    @Test
+    void toolResponseToolsSpec(){
+        assertThat(RuntimeInfoTool.TOOL_SPEC).isNotNull().as("TOOL_SPEC should not be null");
+        assertThat(RuntimeInfoTool.TOOL_SPEC.get("name")).isNotNull().as("name should not be null");
+        assertThat(RuntimeInfoTool.TOOL_SPEC.get("description")).isNotNull().as("description should not be null");
     }
 }
