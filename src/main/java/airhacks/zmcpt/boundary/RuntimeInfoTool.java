f8b9e23b04924c593eca44ae6fd7cb6d3dfba680
Adam Bien <abien@adam-bien.com>
2025-07-07 09:32:46 +0200

    tool signature described

--- src/main/java/airhacks/zmcpt/boundary/RuntimeInfoTool.java
+++ src/main/java/airhacks/zmcpt/boundary/RuntimeInfoTool.java
@@ -4,20 +4,30 @@ import java.util.Map;
 import java.util.function.Function;
 
 import airhacks.zmcpt.entity.ToolResponse;
-
+/**
+ * An example tool that returns the server's runtime metrics.
+ * It is a starting point for creating your own implementations.
+ * A tool implementation has to:
+ * <ul>
+ * <li>implement the <code>Function<String, Map<String, String>></code> interface</li>
+ * <li>return a <code>Map<String, String></code> with "content" and "error" keys</li>
+ * <li>have a <code>TOOL_SPEC</code> attribute that describes the tool's input parameters and its behavior</li>
+ * </ul>
+ */
 public class RuntimeInfoTool implements Function<String, Map<String, String>> {
 
     /**
      * A TOOL_SPEC attribute has to exist in every tool implementation.
      * It is used to describe the tool's input parameters and its behavior and is fetched by zmcp via reflection.
      */
-    static Map<String, String> TOOL_SPEC = ToolSpec.SINGLE_REQUIRED_PARAMETER;
+    static Map<String, String> TOOL_SPEC = ToolSpec.singleRequiredParameter("RuntimeInfoTool", "returns server's runtime metrics");
 
     /**
      * The apply method is the entry point for the tool.
      * It is called by zmcp when the tool is invoked.
      * The parameter is the input parameter and is usually in JSON format.
-     * The return value is a map of key-value pairs with predefined keys.
+     * The return value is a map of key-value pairs with "content" and "error" keys.
+     * The "content" key is the tool's response and the "error" key is a boolean value indicating if the tool failed.
      */
     @Override
     public Map<String, String> apply(String parameter) {
