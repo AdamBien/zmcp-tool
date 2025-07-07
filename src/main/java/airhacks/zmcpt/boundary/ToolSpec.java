40dc608033fffe08f59d2a2c069fa753379157d7
Adam Bien <abien@adam-bien.com>
2025-07-07 09:31:54 +0200

    parameterised description introduced

--- src/main/java/airhacks/zmcpt/boundary/ToolSpec.java
+++ src/main/java/airhacks/zmcpt/boundary/ToolSpec.java
@@ -2,19 +2,26 @@ package airhacks.zmcpt.boundary;
 
 import java.util.Map;
 
+/**
+ * Returns the response for "tools/list" request 
+ * as specified by the MCP protocol: https://modelcontextprotocol.io/specification/2025-03-26/server/tools#protocol-messages
+ */
 public interface ToolSpec {
-    Map<String,String> SINGLE_REQUIRED_PARAMETER =              Map.of(
-            "name", "RuntimeInfoTool",
-            "description", "returns server's runtime metrics",
-            "inputSchema", """
-                    {
-                        "type": "object",
-                        "properties": {
-                            "input": {
-                                "type": "string"
-                            }
-                        },
-                        "required": ["input"]
-                    }
-            """);
+
+    static Map<String, String> singleRequiredParameter(String name, String description) {
+        return Map.of(
+                "name", name,
+                "description", description,
+                "inputSchema", """
+                                {
+                                    "type": "object",
+                                    "properties": {
+                                        "input": {
+                                            "type": "string"
+                                        }
+                                    },
+                                    "required": ["input"]
+                                }
+                        """);
+    }
 }
